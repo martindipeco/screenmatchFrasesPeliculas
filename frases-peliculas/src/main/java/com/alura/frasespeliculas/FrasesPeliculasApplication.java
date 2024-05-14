0403860badfcf2e24b127ca965945af29c74c68d
martindipeco <martindipeco@gmail.com>
2024-05-14 11:38:52 -0300

    first commit

---
+++ frases-peliculas/src/main/java/com/alura/frasespeliculas/FrasesPeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.alura.frasespeliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrasesPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrasesPeliculasApplication.class, args);
+	}
+
+}
---
+++ frases-peliculas/src/test/java/com/alura/frasespeliculas/FrasesPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.frasespeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrasesPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
